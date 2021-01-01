ec682fd843c8c02044af05a23b20a4eb1d15ff42
dakshvora <72599084+dakshvora@users.noreply.github.com>
2021-01-01 12:32:54 +0530

    Add files via upload

---
+++ Rope.js
@@ -0,0 +1,25 @@
+class Rope {
+    constructor(body1, body2){
+        var options = {
+            bodyA : body1,
+            bodyB : body2,
+            length : 100,
+            stiffness : 0
+        }
+
+        this.body = Matter.Constraint.create(options);
+
+        World.add(world, this.body);
+    }
+
+    display() {
+
+        var pointA = this.body.bodyA.position;
+        var pointB = this.body.bodyB.position;
+        push ();
+        strokeWeight(5);
+        stroke("brown");
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+        pop ();
+    }
+}
